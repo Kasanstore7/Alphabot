0d706963f7f160ca0b64ce4dacca51c2cb753fdd
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-10-30 17:10:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,30 +26,30 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Herobot-MD" //namabot kalian
-global.ownername= "HaqzzXD" //nama kalian
+global.botname = "SaxiaBot-Mdv2" //namabot kalian
+global.ownername= "® Ka'ꭙࣼell" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/haqzzxd" //bebas
-global.email = "haqzganz@gmail.com" //bebas
+global.email = "sannstore272@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6281333741517'] //premium user
-global.owner = ["628155174757","6281333741517"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "628155174757" // nomor wa kalian
-global.ownernomerr = "+628155174757" //nmr wa kalian
+global.premium = ['6285641142178'] //premium user
+global.owner = ["6285641142178","6283805685278"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285641142178" // nomor wa kalian
+global.ownernomerr = "++1 (450)3239042" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = 'SaxiaBotzV2' //sticker wm ubah
+global.author = 'Di Buat Oleh Saxia' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
 global.limitawal = {
-    premium: "10000",
-    free: 100
+    premium: "9999999",
+    free: 10
 }
 
 global.adventureRPG = 'https://telegra.ph/file/ff94536d69e0f4f3e7b54.jpg'
